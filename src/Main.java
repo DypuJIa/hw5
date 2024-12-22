87878ea2f81fb68cffc10190bf6c657ceaa1fb98
origin <choriev.dmitry647@gmail.com>
2024-12-22 20:02:08 +0300

    home work №5 fixed

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,19 @@ public class Main {
         int clientDeviceYear = 2015;
         if (clientDeviceYear < 2015 && clientOS == 0) {
             System.out.println(" Установите облегченную версию приложения для iOS по ссылке");
-        }else if (clientDeviceYear >= 2015 && clientOS == 0) {
+        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
             System.out.println(" Установите обычную версию приложения для iOS по ссылке");
         } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println(" Установите облегченную версию приложения для Android по ссылке ");
-        }else{
+        } else {
             System.out.println(" Установите обычную версию приложения для Android по ссылке ");
         }
 
         System.out.println(" Задача №3");
         int year = 2015;
-        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)){
-        System.out.println(year + " год високосный ");
-    }else {
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            System.out.println(year + " год високосный ");
+        } else {
             System.out.println(year + " год не является високосным");
         }
 
